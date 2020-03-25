6b07b5c335c6d561a6fb80224edd1989e94ff485
shifali.choubey <shifali.choubey@codilime.com>
2020-03-25 16:52:57 +0100

    changed response structure for v3 schema

--- keystone.go
+++ keystone.go
@@ -8,10 +8,9 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"strings"
-	"time"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 // KeystoneClient is a client of the OpenStack Keystone service that adds authentication
@@ -23,7 +22,10 @@ type KeystoneClient struct {
 	osPassword   string
 	osAdminToken string
 
-	current *KeystoneToken
+	tokenID    string
+	expiresAt  string
+	issuedAt   string
+	isv3Client bool
 }
 
 type KeepaliveKeystoneClient struct {
@@ -44,6 +46,43 @@ type KeystoneToken struct {
 	Issued_At string
 }
 
+type KeystoneTokenv3 struct {
+	Token struct {
+		Methods []string `json:"methods"`
+		Roles   []struct {
+			ID   string `json:"id"`
+			Name string `json:"name"`
+		} `json:"roles"`
+		System struct {
+			All bool `json:"all"`
+		} `json:"system"`
+		ExpiresAt time.Time `json:"expires_at"`
+		Catalog   []struct {
+			Endpoints []struct {
+				RegionID  string `json:"region_id"`
+				URL       string `json:"url"`
+				Region    string `json:"region"`
+				Interface string `json:"interface"`
+				ID        string `json:"id"`
+			} `json:"endpoints"`
+			Type string `json:"type"`
+			ID   string `json:"id"`
+			Name string `json:"name"`
+		} `json:"catalog"`
+		User struct {
+			PasswordExpiresAt interface{} `json:"password_expires_at"`
+			Domain            struct {
+				ID   string `json:"id"`
+				Name string `json:"name"`
+			} `json:"domain"`
+			ID   string `json:"id"`
+			Name string `json:"name"`
+		} `json:"user"`
+		AuditIds []string  `json:"audit_ids"`
+		IssuedAt time.Time `json:"issued_at"`
+	} `json:"token"`
+}
+
 // NewKeystoneClient allocates and initializes a KeystoneClient
 func NewKeystoneClient(auth_url, tenant_name, username, password, token string) *KeystoneClient {
 	return &KeystoneClient{
@@ -52,44 +91,32 @@ func NewKeystoneClient(auth_url, tenant_name, username, password, token string) 
 		username,
 		password,
 		token,
-		nil,
+		"",
+		"",
+		"",
+		false,
 	}
 }
 
 func NewKeepaliveKeystoneClient(auth_url, tenant_name, username, password, token string) *KeepaliveKeystoneClient {
-	return &KeepaliveKeystoneClient {
+	return &KeepaliveKeystoneClient{
 		KeystoneClient{
 			auth_url,
 			tenant_name,
 			username,
 			password,
 			token,
-			nil,
+			"",
+			"",
+			"",
+			false,
 		},
 	}
 }
 
 // Authenticate sends an authentication request to keystone.
-func (kClient *KeystoneClient) Authenticate() error {
-	// identity:CredentialType
-	type AuthTokenRequest struct {
-		Auth struct {
-			Token struct {
-				Id string `json:"id"`
-			} `json:"token"`
-		} `json:"auth"`
-	}
-	type AuthCredentialsRequestv2 struct {
-		Auth struct {
-			TenantName          string `json:"tenantName"`
-			PasswordCredentials struct {
-				Username string `json:"username"`
-				Password string `json:"password"`
-			} `json:"passwordCredentials"`
-		} `json:"auth"`
-	}
-
-	//Credential request for v3 schema
+func (kClient *KeystoneClient) AuthenticateV3() error {
+	kClient.isv3Client = true
 	type AuthCredentialsRequestv3 struct {
 		Auth struct {
 			Identity struct {
@@ -104,6 +131,78 @@ func (kClient *KeystoneClient) Authenticate() error {
 					} `json:"user"`
 				} `json:"password"`
 			} `json:"identity"`
+			Scope struct {
+				System struct {
+					All bool `json:"all"`
+				} `json:"system"`
+			} `json:"scope"`
+		} `json:"auth"`
+	}
+
+	url := kClient.osAuthURL
+	if url[len(url)-1] != '/' {
+		url += "/"
+	}
+	url += "tokens"
+
+	var data []byte
+	var err error
+	request := AuthCredentialsRequestv3{}
+	request.Auth.Identity.Password.User.Name = kClient.osUsername
+	request.Auth.Identity.Password.User.Password = kClient.osPassword
+	request.Auth.Identity.Password.User.Domain.ID = "default"
+	request.Auth.Identity.Methods = append(request.Auth.Identity.Methods, "password")
+	request.Auth.Scope.System.All = true
+	if data, err = json.Marshal(&request); err != nil {
+		return err
+	}
+
+	resp, err := http.Post(url, "application/json",
+		bytes.NewReader(data))
+
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated {
+		return fmt.Errorf("%s: %s", resp.Status, body)
+	}
+
+	var response KeystoneTokenv3
+	err = json.Unmarshal(body, &response)
+	if err != nil {
+		return err
+	}
+	kClient.tokenID = resp.Header.Get("X-Subject-Token")
+	kClient.issuedAt = response.Token.IssuedAt.String()
+	kClient.expiresAt = response.Token.ExpiresAt.String()
+	return nil
+
+}
+
+// Authenticate sends an authentication request to keystone.
+func (kClient *KeystoneClient) Authenticate() error {
+	// identity:CredentialType
+	type AuthTokenRequest struct {
+		Auth struct {
+			Token struct {
+				Id string `json:"id"`
+			} `json:"token"`
+		} `json:"auth"`
+	}
+	type AuthCredentialsRequest struct {
+		Auth struct {
+			TenantName          string `json:"tenantName"`
+			PasswordCredentials struct {
+				Username string `json:"username"`
+				Password string `json:"password"`
+			} `json:"passwordCredentials"`
 		} `json:"auth"`
 	}
 
@@ -125,9 +224,6 @@ func (kClient *KeystoneClient) Authenticate() error {
 	}
 	url += "tokens"
 
-	parts := strings.Split(url, "/")
-	schemaversion := parts[3]
-
 	var data []byte
 	var err error
 	if len(kClient.osAdminToken) > 0 {
@@ -135,22 +231,13 @@ func (kClient *KeystoneClient) Authenticate() error {
 		request.Auth.Token.Id = kClient.osAdminToken
 		data, err = json.Marshal(&request)
 	} else {
-		if schemaversion == "v3" {
-			request := AuthCredentialsRequestv3{}
-			request.Auth.Identity.Password.User.Name = kClient.osUsername
-			request.Auth.Identity.Password.User.Password = kClient.osPassword
-			request.Auth.Identity.Password.User.Domain.ID = "default"
-			request.Auth.Identity.Methods = append(request.Auth.Identity.Methods, "password")
-			data, err = json.Marshal(&request)
-		} else {
-			request := AuthCredentialsRequestv2{}
-			request.Auth.PasswordCredentials.Username =
-				kClient.osUsername
-			request.Auth.PasswordCredentials.Password =
-				kClient.osPassword
-			request.Auth.TenantName = kClient.osTenantName
-			data, err = json.Marshal(&request)
-		}
+		request := AuthCredentialsRequest{}
+		request.Auth.PasswordCredentials.Username =
+			kClient.osUsername
+		request.Auth.PasswordCredentials.Password =
+			kClient.osPassword
+		request.Auth.TenantName = kClient.osTenantName
+		data, err = json.Marshal(&request)
 	}
 
 	if err != nil {
@@ -171,7 +258,7 @@ func (kClient *KeystoneClient) Authenticate() error {
 		return err
 	}
 
-	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated{
+	if resp.StatusCode != http.StatusOK {
 		return fmt.Errorf("%s: %s", resp.Status, body)
 	}
 
@@ -181,22 +268,23 @@ func (kClient *KeystoneClient) Authenticate() error {
 		return err
 	}
 
-	kClient.current = new(KeystoneToken)
-	*kClient.current = response.Access.Token
+	kClient.expiresAt = response.Access.Token.Expires
+	kClient.issuedAt = response.Access.Token.Issued_At
+	kClient.tokenID = response.Access.Token.Id
 	return nil
 }
 
 func (kClient *KeepaliveKeystoneClient) needsRefreshing() (bool, error) {
-	if kClient.current == nil {
+	if len(kClient.tokenID) == 0 {
 		return true, nil
 	}
 
-	issuedAtTime, err := time.Parse(time.RFC3339, kClient.current.Issued_At)
+	issuedAtTime, err := time.Parse(time.RFC3339, kClient.issuedAt)
 	if err != nil {
 		return false, err
 	}
 
-	expires, err := time.Parse(time.RFC3339, kClient.current.Expires)
+	expires, err := time.Parse(time.RFC3339, kClient.expiresAt)
 	if err != nil {
 		return false, err
 	}
@@ -213,7 +301,7 @@ func (kClient *KeepaliveKeystoneClient) AddAuthentication(req *http.Request) err
 	}
 
 	if needsRefreshing {
-		kClient.current = nil
+		kClient.tokenID = ""
 	}
 
 	return kClient.KeystoneClient.AddAuthentication(req)
@@ -221,12 +309,17 @@ func (kClient *KeepaliveKeystoneClient) AddAuthentication(req *http.Request) err
 
 // AddAuthentication adds the authentication data to the HTTP header.
 func (kClient *KeystoneClient) AddAuthentication(req *http.Request) error {
-	if kClient.current == nil {
-		err := kClient.Authenticate()
-		if err != nil {
-			return err
+	if len(kClient.tokenID) == 0 {
+		if kClient.isv3Client {
+			if err := kClient.AuthenticateV3(); err != nil {
+				return err
+			}
+		} else {
+			if err := kClient.Authenticate(); err != nil {
+				return err
+			}
 		}
 	}
-	req.Header.Set("X-Auth-Token", kClient.current.Id)
+	req.Header.Set("X-Auth-Token", kClient.tokenID)
 	return nil
 }
